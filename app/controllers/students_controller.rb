22a3485e9ee4b9aa486d1031a233c036bfc2bc58
roger chat <mattrogerpelosi@gmail.com>
2021-10-12 12:43:50 -0400

    commit tree 78675e5d4e80fc99a0665a96696ee31286dc112a

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,25 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = []
+
+    if params[:name]
+      Student.all.each do |student|
+        if student[:first_name].include?(params[:name]) 
+          students << student
+        elsif student[:last_name].include?(params[:name])
+          students << student
+        end
+      end
+    else students << Student.all 
+    end
+
     render json: students
+  end
+
+  def show 
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
